340d635371705bfcd52d9d526326a89bae68d85a
Isreal6ix <isrealsix@gmail.com>
2021-08-03 21:58:07 +0700

    used useEffect to handle a side effect to send data from Redux to DB

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
+import { useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
 
 function App() {
+	const cart = useSelector(state => state.cart);
+
+	useEffect(() => {
+		fetch('https://appseven-9144b-default-rtdb.firebaseio.com/cart.json', {
+			method: 'PUT',
+			body: JSON.stringify(cart),
+		});
+	}, [cart]);
+
 	const showCart = useSelector(state => state.ui.cartIsVisible);
 	return (
 		<Layout>
