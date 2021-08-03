736bd023707be0e8d2d93aaa21d03a5a91d481b9
Isreal6ix <isrealsix@gmail.com>
2021-08-03 20:39:58 +0700

    used redux to access all data throuh different components

--- src/components/Shop/Products.js
+++ src/components/Shop/Products.js
@@ -1,19 +1,37 @@
 import ProductItem from './ProductItem';
 import classes from './Products.module.css';
 
-const Products = (props) => {
-  return (
-    <section className={classes.products}>
-      <h2>Buy your favorite products</h2>
-      <ul>
-        <ProductItem
-          title='Test'
-          price={6}
-          description='This is a first product - amazing!'
-        />
-      </ul>
-    </section>
-  );
+const DUMMY_PRODUCTS = [
+	{
+		id: 'p1',
+		price: 6,
+		title: 'My First Book',
+		description: 'The interesting Book',
+	},
+	{
+		id: 'p2',
+		price: 4,
+		title: 'My First Love',
+		description: 'The interesting Book of love',
+	},
+];
+const Products = props => {
+	return (
+		<section className={classes.products}>
+			<h2>Buy your favorite products</h2>
+			<ul>
+				{DUMMY_PRODUCTS.map(product => (
+					<ProductItem
+						key={product.id}
+						title={product.title}
+						price={product.price}
+						id={product.id}
+						description={product.description}
+					/>
+				))}
+			</ul>
+		</section>
+	);
 };
 
 export default Products;
